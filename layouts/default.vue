7a0258bb8ccb1bf389c45768d5f6ddfa4d454866
sculs2255 <60909236+sculs2255@users.noreply.github.com>
2021-07-15 11:03:04 +0700

    15/07

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,24 @@
 <template>
   <v-app dark>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-toolbar-title v-text="title" />
       <v-tabs align-with-title>
-            <v-tab>Status Case</v-tab>
-            <v-tab>My case</v-tab>
-            <v-tab>Master</v-tab>
-          </v-tabs>
+        <v-tab>Status Case</v-tab>
+        <v-tab>My case</v-tab>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-tab color="primary" dark v-bind="attrs" v-on="on">
+              Master
+            </v-tab>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-tabs>
+
       <v-spacer />
     </v-app-bar>
     <v-main>
@@ -18,10 +26,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -29,28 +34,30 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          name: "status",
+          title: "Inspire",
+          to: "/status-page"
+        },
+        { title: "Master",
+          to: "/master"},
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      title: "Vuetify.js"
+    };
   }
-}
+};
 </script>
--- pages/status-page.vue
+++ pages/status-page.vue
@@ -1,5 +1,56 @@
 <template>
-<div>
-    <h1> SUn</h1>
+<div id="app">
+  <v-app id="inspire">
+    <v-tabs dark grow>
+      <v-toolbar color="cyan" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Page title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-tabs-bar class="cyan" slot="extension">
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+          <v-tabs-item
+            v-for="(item, i) in items"
+            :key="i"
+            :href="'#tab-' + (i + 1)"
+          >
+            {{ item }}
+          </v-tabs-item>
+          <v-menu :nudge-width="100" left bottom v-if="more.length">
+            <v-tabs-item slot="activator">
+              Menu
+              <v-icon>arrow_drop_down</v-icon>
+            </v-tabs-item>
+            <v-list class="grey lighten-3">
+              <v-list-tile
+                tag="a"
+                v-for="item in more"
+                :key="item"
+                @click="addItem(item)"
+              >
+                {{ item }}
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+        </v-tabs-bar>
+      </v-toolbar>
+      <v-tabs-items>
+        <v-tabs-content
+          v-for="i in 5"
+          :key="i"
+          :id="'tab-' + i"
+        >
+          <v-card flat>
+            <v-card-text>{{ text }}</v-card-text>
+          </v-card>
+        </v-tabs-content>
+      </v-tabs-items>
+    </v-tabs>
+  </v-app>
 </div>
 </template>
